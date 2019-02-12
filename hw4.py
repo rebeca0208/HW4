c9bc9e30954edcc7695ab34664932e98e9ffb95e
rebeca0208 <45616122+rebeca0208@users.noreply.github.com>
2019-02-11 23:19:24 -0500

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,22 @@
+# Dependencies and Setup
+import pandas as pd
+import numpy as np
+
+# File to Load (Remember to Change These)
+school_data_to_load = "Resources/schools_complete.csv"
+student_data_to_load = "Resources/students_complete.csv"
+
+# Read School and Student Data File and store into Pandas Data Frames
+school_data = pd.read_csv(school_data_to_load)
+student_data = pd.read_csv(student_data_to_load)
+
+# Combine the data into a single dataset
+school_data_complete = pd.merge(student_data, school_data, how="left", on=["school_name", "school_name"])
+
+# Sample bins. Feel free to create your own bins.
+spending_bins = [0, 585, 615, 645, 675]
+group_names = ["<$585", "$585-615", "$615-645", "$645-675"]
+
+# Sample bins. Feel free to create your own bins.
+size_bins = [0, 1000, 2000, 5000]
+group_names = ["Small (<1000)", "Medium (1000-2000)", "Large (2000-5000)"]
